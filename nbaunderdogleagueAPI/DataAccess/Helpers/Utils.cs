3cf0029ec518b26b151de62fb43f7e26f400126b
rudymiked <rudymiked@gmail.com>
2022-11-26 13:18:32 -0800

    updated scoring

--- nbaunderdogleagueAPI/DataAccess/Helpers/Utils.cs
+++ nbaunderdogleagueAPI/DataAccess/Helpers/Utils.cs
@@ -5,7 +5,7 @@
         public static double CalculateScore(int ProjectedWin, int ProjectedLoss, int Wins, int Losses, int PlayoffWins = 0)
         {
             double projectedDiff = (double)(ProjectedWin / (double)(ProjectedWin + ProjectedLoss));
-            double actualDiff = (double)(Wins / (double)(Wins + Losses));
+            double actualDiff = (double)((Wins + PlayoffWins) / (double)(Wins + Losses)); // bonus for playoff wins
             double score = (double)(actualDiff / (double)projectedDiff);
 
             return double.IsNaN(Math.Round(score, 2)) ? 0.0 : Math.Round(score, 2);
